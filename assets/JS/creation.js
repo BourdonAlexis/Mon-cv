aa39260f06d83c833efd08d8cba752e3d491927d
BourdonAlexis <alexisbourdon@hotmail.fr>
2021-11-02 15:04:55 +0100

    Modif page CV :  - téléchargement du CV
    Ajout page création :
     - Ajout des onglets
     - Ajout JS page création

---
+++ assets/JS/creation.js
@@ -0,0 +1,22 @@
+function openCity(evt, tabName) {
+    // Declare toutes les variables
+    var i, tabcontent, tablinks;
+
+    // Obtiens tout les elements avec la class="tabcontent" et les caches
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Obtiens tout les elements avec la class="tablinks" et retire la classe "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    // regarde la tab courante , et ajoutez une classe "active" au bouton qui a ouvert l'onglet
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+
+
+}
